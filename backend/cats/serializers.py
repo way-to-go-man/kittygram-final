d05c7221e14e1b31098f5fa2e761352282c8f2b1
Vladislav Bondarev <v.bondarev@sreda.dev>
2026-04-27 14:03:46 +0300

    qick back fix

--- backend/cats/serializers.py
+++ backend/cats/serializers.py
@@ -34,11 +34,13 @@ class Base64ImageField(serializers.ImageField):
         if isinstance(data, str) and data.startswith('data:image'):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
-
             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
         return super().to_internal_value(data)
 
+    def to_representation(self, value):
+        if not value:
+            return None
+        return '/media/' + str(value)
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(required=False, many=True)
