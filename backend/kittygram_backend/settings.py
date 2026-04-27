fbf78a6882ed6d1a40e3257dde80de2ad0cf7979
Vladislav Bondarev <v.bondarev@sreda.dev>
2026-04-27 11:15:53 +0300

    Add Dockerfile, docker-compose, nginx config, CI/CD workflow

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -54,14 +54,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
+        'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
